757c9e9ccb7e97b7e30ff3056e29e3ac0b95825c
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-03-07 12:00:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,26 @@ result = cv2.warpPerspective(img, matrix, (1000, 1000))
 cv2.imshow('frame', img)  # Initial Capture
 cv2.imshow('frame1', result)  # Transformed Capture
 
-cv2.waitKey(0)
+cv2.waitKey(5000)
 cv2.destroyAllWindows()
+
+
+
+gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLines(edges,1,np.pi/180,200)
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+cv2.imwrite('houghlines3.jpg',img)
+
