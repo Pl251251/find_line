eb1abdf98c2b5d9b30a558ece36ec95a4d200fb6
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-03-17 11:32:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("image3.jpg",1)
+img = cv2.imread("image1.jpg",1)
 
 height = img.shape[0]
 width = img.shape[1]
 
 #perspective transform
-pts1 = np.float32([[760, 2003], [2303, 2003],[104, 3209], [2915, 3209]])
+pts1 = np.float32([[760, 2003], [2300, 2003],[248, 2923], [2805, 2923]])
 pts2 = np.float32([[0, 0], [400, 0], [0, 400], [400, 400]])
 
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
@@ -20,7 +20,7 @@ gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
 #detect lines
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 185, None, 0, 0)
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 165, None, 0, 0)
 
 #draw lines
 c=1000
@@ -34,7 +34,8 @@ for i in lines:
     y0 = b * rho
     pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
     pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-    cv2.line(result, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
+    #cv2.line(result, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
+
 
     if (x0<c):
         line1 =i
@@ -53,7 +54,11 @@ for i in lst:
     b = np.sin(theta)
     x1 = a * rho
     y1 = b * rho
+    pt1 = (int(x1 + 1000 * (-b)), int(y1 + 1000 * (a)))
+    pt2 = (int(x1 - 1000 * (-b)), int(y1 - 1000 * (a)))
     lst2.append([a, b, x1, y1])
+    cv2.line(result, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
+    cv2.circle(result,(int(x1),int(y1)),10,(255,0,0),10)
 
 info1 =(lst2[1][2] + lst2[0][2])/2
 info2 = (lst2[1][2]+lst2[0][2])/2
