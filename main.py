c559abac37f344d09e87c07d6946f8662c0fadce
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-03-18 06:35:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("image1.jpg",1)
+img = cv2.imread("image4.jpg",1)
 
 height = img.shape[0]
 width = img.shape[1]
+print(height)
+print(width)
 
 #perspective transform
-pts1 = np.float32([[760, 2003], [2300, 2003],[248, 2923], [2805, 2923]])
+#pts1 = np.float32([[760, 2003], [2300, 2003],[248, 2923], [2805, 2923]])
+pts1 = np.float32([[840, 1300], [2100, 1300],[350, 2523], [2605, 2523]])
 pts2 = np.float32([[0, 0], [400, 0], [0, 400], [400, 400]])
 
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 #warp image
 result = cv2.warpPerspective(img, matrix, (400, 400))
 
+
 #gray and edge
 gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
 #detect lines
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 165, None, 0, 0)
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 144, None, 0, 0)
 
 #draw lines
-c=1000
+c=9000
 d=0
 for i in lines:
     rho = i[0][0]
@@ -57,8 +61,8 @@ for i in lst:
     pt1 = (int(x1 + 1000 * (-b)), int(y1 + 1000 * (a)))
     pt2 = (int(x1 - 1000 * (-b)), int(y1 - 1000 * (a)))
     lst2.append([a, b, x1, y1])
-    cv2.line(result, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
-    cv2.circle(result,(int(x1),int(y1)),10,(255,0,0),10)
+    cv2.line(result, pt1, pt2, (255, 0, 0), 1, cv2.LINE_AA)
+    #cv2.circle(result,(int(x1),int(y1)),10,(255,0,0),10)
 
 info1 =(lst2[1][2] + lst2[0][2])/2
 info2 = (lst2[1][2]+lst2[0][2])/2
